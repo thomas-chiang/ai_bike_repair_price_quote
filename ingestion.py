92f5a56cc70ef9cab3d506ae0facd071b6d677f8
Thomas Chiang <t7hiang@gmail.com>
2025-06-17 22:35:51 +0800

    feat: pinecore

--- ingestion.py
+++ ingestion.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
+# from langchain_chroma import Chroma
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
 
 
 load_dotenv()
@@ -25,6 +27,13 @@ text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
 )
 doc_splits = text_splitter.split_documents(docs_list)
 
+embedding = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+
+# PineconeVectorStore.from_documents(
+#     documents=doc_splits, 
+#     embedding=embedding, 
+#     index_name="superbench")
+
 
 # vectorstore = Chroma.from_documents(
 #     documents=doc_splits,
@@ -34,10 +43,12 @@ doc_splits = text_splitter.split_documents(docs_list)
 # )
 
 
-retriever = Chroma(
-    collection_name="rag-chroma",
-    persist_directory="./.chroma",
-    embedding_function=GoogleGenerativeAIEmbeddings(model="models/embedding-001"),
-).as_retriever()
+# retriever = Chroma(
+#     collection_name="rag-chroma",
+#     persist_directory="./.chroma",
+#     embedding_function=GoogleGenerativeAIEmbeddings(model="models/embedding-001"),
+# ).as_retriever()
+
+retriever = PineconeVectorStore(index_name="superbench", embedding=embedding).as_retriever()
 
 
